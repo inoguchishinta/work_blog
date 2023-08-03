d406d39447afe03b07e21d4cc25e595afd765f33
shinta_grabss <124234522+inoguchishinta@users.noreply.github.com>
2023-08-03 13:03:08 +0900

    Refactor/method (#33)
    * Refac: articles_controllerファイルのmethod配置順を変更
    
    * Refac: comments_controllerファイルのmethod順を変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,31 +2,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def edit
     @article = Article.find(params[:id])
   end
-    
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    
-    redirect_to articles_path
-  end
-  def update
-    @article = Article.find(params[:id])
-    
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render 'edit'
-    end
-  end
+
   def create
     @article = Article.new(article_params)
     
@@ -36,7 +24,24 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    @article = Article.find(params[:id])
     
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text, :status)
